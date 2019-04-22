79f0babb6658e52d84156990716c7098ac416b47
bryant <bchhun@gmail.com>
2019-04-22 13:01:35 -0700

    clean up for distribution

--- decon_ivan_data_multiproc.py
+++ decon_ivan_data_multiproc.py
@@ -3,9 +3,13 @@ import tifffile
 import os
 import numpy as np
 
+import multiprocessing
+
+# https://stackoverflow.com/questions/20887555/dead-simple-example-of-using-multiprocessing-queue-pool-and-locking
+
 dir = "Z:\\ComputationalMicroscopy\\SpinningDisk\\Processed\\Decon_raw"
-# experiment = "\\2019_04_08_U2OS_plin2_Lipid_MT_Actin_3"
-experiment = "\\2019_04_08_U2OS_plin2_Lipid_MT_Actin_Slide2_2"
+experiment = "\\2019_04_08_U2OS_plin2_Lipid_MT_Actin_3"
+# experiment = "\\2019_04_08_U2OS_plin2_Lipid_MT_Actin_Slide2_2"
 outdir = "Z:\\ComputationalMicroscopy\\SpinningDisk\\Processed\\Decon_processed\\SIM_PSF"+experiment
 if not os.path.exists(outdir):
     os.mkdir(outdir)
@@ -21,17 +25,16 @@ channels = {'488': '\\Zyla_488_Widefield_tiffstack.tif',
 
 positions = ['\\'+name for name in os.listdir(dir+experiment)
               if os.path.isdir(os.path.join(dir+experiment, name))]
+positions_set = {'\\'+name for name in os.listdir(dir+experiment)
+              if os.path.isdir(os.path.join(dir+experiment, name))}
 
 wavelengths = {'405': 430,
                '488': 515,
                '561': 585,
                '637': 655}
 
-# set range on position if want a subset
-for pos in positions[:]:
-    if not os.path.exists(outdir+pos):
-        os.mkdir(outdir+pos)
 
+def proc_channel(pos):
     for chan in channels:
         data = dir+experiment+pos+channels[chan]
         psf = dir+"\\"+kernels[chan]
@@ -51,3 +54,20 @@ for pos in positions[:]:
             out_filename = '\\img_000000000_Zyla_%s_Widefield_%03d.tif' % (chan, zIdx)
             tifffile.imsave(outdir+pos+out_filename, result[zIdx].astype(dtype=np.uint16))
 
+
+def mp_worker(pos):
+    print('========== processing position %s ==========' % pos)
+    if not os.path.exists(outdir + pos):
+        os.mkdir(outdir + pos)
+    proc_channel(pos)
+    print("========== Process \tDONE ==========")
+
+
+# subset positions if you want to try on only a few
+def mp_handler():
+    p = multiprocessing.Pool(3)
+    p.map(mp_worker, positions[:])
+
+
+if __name__ == '__main__':
+    mp_handler()
